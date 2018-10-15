6bc0799c46c042f8d66ac666b1f3d14a0ea334de
AlperenTemur <38461860+AlperenTemur@users.noreply.github.com>
2018-10-16 02:14:22 +0300

    Update list intersection.py

--- Lists/list intersection.py
+++ Lists/list intersection.py
@@ -1,1 +1,11 @@
 
+def intersection1(list1,list2):
+	return list(set(list1)&set(list2))
+
+def intersection2(list1,list2):
+ """Duplicates elements that's duplicated in list2"""
+	return [x for x in list1 if x in list2]
+
+def intersection3(list1,list2):
+	short, long = (list1, list2) if len(list1) < len(list2) else (list2, list1)
+	return list(set(short).intersection(long))
