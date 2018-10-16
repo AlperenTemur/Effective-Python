94420450f548a5ca5901efd97f9c648b7dd9318a
AlperenTemur <38461860+AlperenTemur@users.noreply.github.com>
2018-10-16 08:35:00 +0300

    Update list intersection.py

--- Lists/list intersection.py
+++ Lists/list intersection.py
@@ -7,5 +7,6 @@ def intersection2(list1,list2):
 	return [x for x in list1 if x in list2]
 
 def intersection3(list1,list2):
+"""This is fastest and most memory efficient one for long lists"""
 	short, long = (list1, list2) if len(list1) < len(list2) else (list2, list1)
 	return list(set(short).intersection(long))
